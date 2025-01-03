e83b6236493ef458b73cce835aa9572720e4c409
mconley91 <matthew.e.conley@gmail.com>
2025-01-03 18:05:06 -0500

    completed sorted or not evaluation

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,17 @@
 puts "Enter a set of numbers separated by commas to begin sorting:"
-unsorted_array = gets.chomp.split("")
+input_array = gets.chomp.split(",")
 
-def bubble_sorter(unsorted_array)
-  
+def is_sorted?(array)
+  evaluating_arr = []
+  array.each_with_index{|item,index| evaluating_arr.push(index != array.length-1 ? item.to_i < array[index+1].to_i : true)}
+  p evaluating_arr.all?
+  evaluating_arr.all?
+end
+
+
+def bubble_sorter(input_array)
 end
 # bubble_sorter("4,3,78,2,0,2")
-bubble_sorter(unsorted_array)
+
+
+is_sorted?(input_array)
