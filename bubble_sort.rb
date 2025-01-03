84f8423c3be35db1deb59ef1dd1f3f87550a66ba
mconley91 <matthew.e.conley@gmail.com>
2025-01-03 17:23:59 -0500

    initial commit, added get variable & method

---
+++ bubble_sort.rb
@@ -0,0 +1,8 @@
+puts "Enter a set of numbers separated by commas to begin sorting:"
+unsorted_array = gets.chomp.split("")
+
+def bubble_sorter(unsorted_array)
+  
+end
+# bubble_sorter("4,3,78,2,0,2")
+bubble_sorter(unsorted_array)
